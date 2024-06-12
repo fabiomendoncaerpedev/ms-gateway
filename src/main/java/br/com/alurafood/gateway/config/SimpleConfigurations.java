aef4e606793a112e1c99d30ba0edffed29807bbc
Fabio Mendonça <fabio.mendonca@totvs.com.br>
2024-06-12 14:14:16 -0300

    fix: created configuration bean in order to solve the DNS problem

---
+++ src/main/java/br/com/alurafood/gateway/config/SimpleConfigurations.java
@@ -0,0 +1,14 @@
+package br.com.alurafood.gateway.config;
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.netty.http.client.HttpClient;
+
+@Configuration
+public class SimpleConfigurations {
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+    }
+}
